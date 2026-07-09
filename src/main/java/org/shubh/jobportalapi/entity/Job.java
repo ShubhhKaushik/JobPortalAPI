b8cb7fd319b064fb8a605782f2a9b0b51b205c11
Shubh Kaushik <shubhkaushik1002@gmail.com>
2026-07-09 18:35:08 +0530

    Started SpringREST project

---
+++ src/main/java/org/shubh/jobportalapi/entity/Job.java
@@ -0,0 +1,70 @@
+package org.shubh.jobportalapi.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="jobs")
+
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String company;
+
+    private String location;
+
+    private Double Salary;
+
+    public Job(){
+
+    }
+
+    public Job(String title, String description, String company, String location, Double Salary) {
+        this.title = title;
+        this.description = description;
+        this.company = company;
+        this.location = location;
+        this.Salary = Salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public Double getSalary() {
+        return Salary;
+    }
+    public void setSalary(Double salary) {
+        Salary = salary;
+    }
+}
---
+++ src/test/java/org/shubh/jobportalapi/JobPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shubh.jobportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
